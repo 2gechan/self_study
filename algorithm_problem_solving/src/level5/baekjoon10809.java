bb4ea29af2e698a689cc4daa53fad9d396b9526b
2gechan <7532602@gmail.com>
2023-09-25 12:27:46 +0900

    백준 문제풀이

---
+++ algorithm_problem_solving/src/level5/baekjoon10809.java
@@ -0,0 +1,39 @@
+package level5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 알파벳 소문자로만 이루어진 단어 S가 주어진다.
+// 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를,
+// 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+public class baekjoon10809 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] testCase = br.readLine().split("");
+        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g",
+                "h", "i", "j", "k", "l", "m", "n", "o", "p",
+                "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        int[] alphabetIndex = new int[26];
+        for (int i = 0; i < alphabetIndex.length; i++) {
+            alphabetIndex[i] = -1;
+        }
+        for (int i = 0; i < testCase.length; i++) {
+            // System.out.println(testCase[i]);
+
+            for (int j = 0; j < alphabet.length; j++) {
+                if(testCase[i].equals(alphabet[j])) {
+                    if(alphabetIndex[j] != -1) {
+                        break;
+                    } else {
+                        alphabetIndex[j] = i;
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < alphabetIndex.length; i++) {
+            System.out.print(alphabetIndex[i] + " ");
+        }
+    }
+}
---
+++ algorithm_problem_solving/src/level5/baekjoon11720.java
@@ -0,0 +1,23 @@
+package level5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+// 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+public class baekjoon11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCaseCount = Integer.parseInt(br.readLine());
+        int sum = 0;
+        String[] str = new String[testCaseCount];
+        str = br.readLine().split("");
+
+        for (int i = 0; i < testCaseCount; i++) {
+            sum += Integer.parseInt(str[i]);
+        }
+        System.out.println(sum);
+    }
+}
