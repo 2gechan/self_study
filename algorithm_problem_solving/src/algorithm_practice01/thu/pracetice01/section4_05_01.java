bf0b806a26a512eed470e8aed72ae9d32e696be8
7532602 <7532602@gmail.com>
2024-07-04 22:31:38 +0900

    목요일 알고리즘 문제풀이

---
+++ algorithm_problem_solving/src/algorithm_practice01/thu/pracetice01/section4_05_01.java
@@ -0,0 +1,51 @@
+package algorithm_practice01.thu.pracetice01;
+
+import java.util.*;
+
+/*
+K번째 큰 수
+현수는 1부터 100사이의 자연수가 적힌 N장의 카드를 가지고 있습니다.
+같은 숫자의 카드가 여러장 있을 수 있습니다.
+현수는 이 중 3장을 뽑아 각 카드에 적힌 수를 합한 값을 기록하려고 합니다.
+3장을 뽑을 수 있는 모든 경우를 기록합니다.
+기록한 값 중 K번째로 큰 수를 출력 하는 프로그램을 작성하세요.
+만약 큰 수부터 만들어진 수가 25 25 23 23 22 20 19......이고 K값이 3이라면 K번째 큰 값 은 22입니다.
+ */
+public class section4_05_01 {
+    static int k;
+
+    static int solution(int[] arr, int n) {
+        int answer = 0;
+        TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int z = j + 1; z < n; z++) {
+                    treeSet.add(arr[i] + arr[j] + arr[z]);
+                }
+            }
+        }
+
+        int cnt = 0;
+        System.out.println(treeSet.last());
+        for (int x : treeSet) {
+            cnt++;
+            if (cnt == k) {
+                answer = x;
+                break;
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(arr, n));
+    }
+}
