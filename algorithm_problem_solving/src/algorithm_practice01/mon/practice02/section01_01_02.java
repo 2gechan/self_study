b22df62f2af1d473f2126c0312bc8ce47f1e8f48
7532602 <7532602@gmail.com>
2024-08-05 23:28:06 +0900

    월요일 알고리즘 문제풀이

---
+++ algorithm_problem_solving/src/algorithm_practice01/mon/practice02/section01_01_02.java
@@ -0,0 +1,22 @@
+package algorithm_practice01.mon.practice02;
+
+import java.util.Scanner;
+
+public class section01_01_02 {
+    static int solution(String testCase, String t) {
+        int answer = 0;
+        testCase = testCase.toLowerCase();
+        char c = t.toLowerCase().charAt(0);
+        for (char x : testCase.toCharArray()) {
+            if (x == c) answer++;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String testCase = sc.nextLine();
+        String t = sc.nextLine();
+        System.out.println(solution(testCase, t));
+    }
+}
