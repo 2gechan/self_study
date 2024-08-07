38c12604d165cec38c266af2c676aab2e32be51a
7532602 <7532602@gmail.com>
2024-08-07 22:41:36 +0900

    수요일 알고리즘 문제풀이

---
+++ algorithm_problem_solving/src/algorithm_practice01/wed/practice02/section3_01_02.java
@@ -0,0 +1,32 @@
+package algorithm_practice01.wed.practice02;
+
+import java.util.Scanner;
+
+public class section3_01_02 {
+    static int[] solution(int[] arr1, int[] arr2) {
+        int[] answer = new int[arr1.length + arr2.length];
+        int a1 = 0, a2 = 0, index = 0;
+        while (a1 < arr1.length && a2 < arr2.length) {
+            if (arr1[a1] < arr2[a2]) answer[index++] = arr1[a1++];
+            else answer[index++] = arr2[a2++];
+        }
+        while (a1 < arr1.length) answer[index++] = arr1[a1++];
+        while (a2 < arr2.length) answer[index++] = arr2[a2++];
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        for (int x : solution(arr1, arr2)) System.out.print(x + " ");
+    }
+}
