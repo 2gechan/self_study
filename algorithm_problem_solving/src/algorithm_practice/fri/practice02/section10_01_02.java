2a5529c4f42f43da10c03378e0897d4c86869dab
7532602 <7532602@gmail.com>
2024-08-16 21:59:22 +0900

    금요일 알고리즘 문제풀이

--- algorithm_problem_solving/src/algorithm_practice/fri/practice02/section10_01_02.java
+++ algorithm_problem_solving/src/algorithm_practice/fri/practice02/section10_01_02.java
@@ -1,2 +1,21 @@
-package algorithm_practice.fri.practice02;public class section10_01_02 {
+package algorithm_practice.fri.practice02;
+
+import java.util.Scanner;
+
+public class section10_01_02 {
+    static int solution(int n) {
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+    }
 }
