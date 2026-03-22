4914fa0f61860469e84de0d92ac13d1c782cdeb9
2gechan <7532602@gmail.com>
2026-03-23 05:53:20 +0900

    one day coding

---
+++ OneDayCoding/codingtest/codingtest/src/baekjoon/silver4/Silver1269.java
@@ -0,0 +1,32 @@
+package baekjoon.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Silver1269 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> set = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (set.contains(num)) set.remove(num);
+            else set.add(num);
+        }
+        System.out.println(set.size());
+    }
+}
