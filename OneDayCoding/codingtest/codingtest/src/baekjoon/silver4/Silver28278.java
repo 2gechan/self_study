33a3c08ecffbbefbb5dc87aae50930742cd4f555
2gechan <7532602@gmail.com>
2026-03-24 22:18:11 +0900

    one day coding

---
+++ OneDayCoding/codingtest/codingtest/src/baekjoon/silver4/Silver28278.java
@@ -0,0 +1,44 @@
+package baekjoon.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Silver28278 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "1":
+                    int num = Integer.parseInt(st.nextToken());
+                    stack.push(num);
+                    break;
+                case "2":
+                    sb.append(!stack.isEmpty() ? stack.pop() : -1).append("\n");
+                    break;
+                case "3":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "4":
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "5":
+                    sb.append(!stack.isEmpty() ? stack.peek() : -1).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
